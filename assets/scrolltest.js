36f7177f9d377c37f91f8882de69ba47a83baefd
Mita Yun <mitayun@gmail.com>
2017-02-23 02:08:23 -0800

    Add small banner. fade in and out menu bar

--- assets/scrolltest.js
+++ assets/scrolltest.js
@@ -1,10 +1,44 @@
-/*var prevScroll = 0;
-var width = $( document ).width();
+var homeBig = false;
+$( document ).ready(function() {
+
+var url = document.URL;
+console.log(url);
+var width = $( document ).width();;
+console.log(width);
 var scrollWidthLimit = 841;
+
+if(url.includes("characters.html") || url.includes("blog.html") || url.includes("about.html") || (width < scrollWidthLimit)) {
+  $(".header_box").css({
+      "display": "none"
+  });
+  $(".header_box_small").css({
+      "display": "block"
+  });
+  homeBig = false;
+} else {
+  $(".header_box").css({
+      "display": "block"
+  });
+  $(".header_box_small").css({
+      "display": "none"
+  });
+  $(".logo_header_bar").css({
+      "opacity": "0"
+  });
+  $("#social_container_small").css({
+      "opacity": "0"
+  });
+  homeBig = true;
+}
+
+});
+
+var prevScroll = 0;
 
 $(window).scroll(function() {
     var wScroll = document.body.scrollTop;
 
+/*
       if(width >= scrollWidthLimit) {
     // bigger divider -> longer it will remain on screen
 
@@ -28,23 +62,39 @@ $(window).scroll(function() {
         "transform": "translate(0px, " + wScroll / 10 + "px)"
     });
   }
+  */
 
 
     if (prevScroll == 0 && wScroll != 0) {
-        // change to white
+        // change to colored background
         $(".header_bar_container").animate({
-            backgroundColor: "white",
-            borderBottomColor: "rgba(211, 211, 211, 0.3)"
-        }, 200);
+            backgroundColor: "#41b0f7",
+        }, 150);
+
+        if(homeBig) {
+          $("#social_container_small").animate({
+              opacity: 1.0,
+          }, 150);
+            $(".logo_header_bar").animate({
+                opacity: 1.0,
+            }, 150);
+        }
 
     } else if (prevScroll != 0 && wScroll == 0) {
         // change to transparent
         $(".header_bar_container").animate({
             backgroundColor: "transparent",
-            borderBottomColor: "rgba(255, 255, 255, 0.3)"
-        }, 200);
+        }, 150);
+
+        if(homeBig) {
+          $("#social_container_small").animate({
+              opacity: 0.0,
+          }, 150);
+            $(".logo_header_bar").animate({
+                opacity: 0.0,
+            }, 150);
+        }
     }
 
     prevScroll = wScroll;
 });
-*/
