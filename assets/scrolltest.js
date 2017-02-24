e7620271999ac392256ed1a3a2046be0be33ed46
Mita Yun <mitayun@gmail.com>
2017-02-23 17:26:40 -0800

    Add favicon. Add preview for all posts. Use preview instead of excerpt.

--- assets/scrolltest.js
+++ assets/scrolltest.js
@@ -1,43 +1,43 @@
 var homeBig = false;
-$( document ).ready(function() {
+$(document).ready(function() {
 
-var url = document.URL;
-console.log(url);
-var width = $( document ).width();;
-console.log(width);
-var scrollWidthLimit = 841;
+    var url = document.URL;
+    console.log(url);
+    var width = $(document).width();;
+    console.log(width);
+    var scrollWidthLimit = 841;
 
-if(url.includes("characters.html") || url.includes("blog.html") || url.includes("about.html") || (width < scrollWidthLimit)) {
-  $(".header_box").css({
-      "display": "none"
-  });
-  $(".header_box_small").css({
-      "display": "block"
-  });
+    if ((url.endsWith(".com/") || url.endsWith("index.html") || url.endsWith(":4000/")) && (width >= scrollWidthLimit)) {
 
-if(width < scrollWidthLimit) {
-  $(".header_box_small").css({
-      "background-image": "url(\"assets/banner-mobile.png\")"
-  });
-}
+        $(".header_box").css({
+            "display": "block"
+        });
+        $(".header_box_small").css({
+            "display": "none"
+        });
+        $(".logo_header_bar").css({
+            "opacity": "0"
+        });
+        $("#social_container_small").css({
+            "opacity": "0"
+        });
+        homeBig = true;
+    } else {
+        $(".header_box").css({
+            "display": "none"
+        });
+        $(".header_box_small").css({
+            "display": "block"
+        });
 
-  homeBig = false;
-} else {
-  $(".header_box").css({
-      "display": "block"
-  });
-  $(".header_box_small").css({
-      "display": "none"
-  });
-  $(".logo_header_bar").css({
-      "opacity": "0"
-  });
-  $("#social_container_small").css({
-      "opacity": "0"
-  });
-  homeBig = true;
-}
+        if (width < scrollWidthLimit) {
+            $(".header_box_small").css({
+                "background-image": "url(\"assets/banner-mobile.png\")"
+            });
+        }
 
+        homeBig = false;
+    }
 });
 
 var prevScroll = 0;
@@ -45,31 +45,31 @@ var prevScroll = 0;
 $(window).scroll(function() {
     var wScroll = document.body.scrollTop;
 
-/*
-      if(width >= scrollWidthLimit) {
-    // bigger divider -> longer it will remain on screen
+    /*
+          if(width >= scrollWidthLimit) {
+        // bigger divider -> longer it will remain on screen
 
 
-    $(".logo").css({
-        "transform": "translate(0px, " + wScroll / 2 + "px)"
-    });
+        $(".logo").css({
+            "transform": "translate(0px, " + wScroll / 2 + "px)"
+        });
 
 
-    $(".layer2").css({
-        "transform": "translate(0px, " + wScroll / 2 + "px)"
-    });
+        $(".layer2").css({
+            "transform": "translate(0px, " + wScroll / 2 + "px)"
+        });
 
 
-    $(".layer3").css({
-        "transform": "translate(0px, " + wScroll / 8 + "px)"
-    });
+        $(".layer3").css({
+            "transform": "translate(0px, " + wScroll / 8 + "px)"
+        });
 
 
-    $(".layer4").css({
-        "transform": "translate(0px, " + wScroll / 10 + "px)"
-    });
-  }
-  */
+        $(".layer4").css({
+            "transform": "translate(0px, " + wScroll / 10 + "px)"
+        });
+      }
+      */
 
 
     if (prevScroll == 0 && wScroll != 0) {
@@ -78,10 +78,10 @@ $(window).scroll(function() {
             backgroundColor: "#41b0f7",
         }, 150);
 
-        if(homeBig) {
-          $("#social_container_small").animate({
-              opacity: 1.0,
-          }, 150);
+        if (homeBig) {
+            $("#social_container_small").animate({
+                opacity: 1.0,
+            }, 150);
             $(".logo_header_bar").animate({
                 opacity: 1.0,
             }, 150);
@@ -93,10 +93,10 @@ $(window).scroll(function() {
             backgroundColor: "transparent",
         }, 150);
 
-        if(homeBig) {
-          $("#social_container_small").animate({
-              opacity: 0.0,
-          }, 150);
+        if (homeBig) {
+            $("#social_container_small").animate({
+                opacity: 0.0,
+            }, 150);
             $(".logo_header_bar").animate({
                 opacity: 0.0,
             }, 150);
