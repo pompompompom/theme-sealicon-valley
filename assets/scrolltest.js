87ce95c47b62711f7cd0fdb340fa31ed0209b431
Mita Yun <mitayun@gmail.com>
2017-03-13 00:17:57 -0700

    make small header. move if statements to html

--- assets/scrolltest.js
+++ assets/scrolltest.js
@@ -22,76 +22,16 @@ $(document).ready(function() {
     console.log(width);
     var scrollWidthLimit = 841;
 
-/*
-    $(".menu_header_text").click(function () {
-        console.log("CLICK");
-        $(".menu_header_text").removeClass("active");
-    $(this).addClass("active");
-    });
-    */
-
-/*
-    $(".menu_header_text").css({
-      "color": "white"
-    });
-    */
-/*
-if (url.endsWith(".com/") || url.endsWith("index.html") || url.endsWith(":4000/")) {
-/*
-  $("#header_text_comic.menu_header_text").css({
-    "color": "red"
-  });
-*/
-/*
-  $("#header_text_comic.menu_header_text").addClass("active");
-} else if(url.endsWith("blog.html")){
-
-  $("#header_text_blog.menu_header_text").css({
-    "color": "red"
-  });
-} else if(url.endsWith("characters.html")) {
-
-  $("#header_text_characters.menu_header_text").css({
-    "color": "red"
-  });
-} else if(url.endsWith("about.html")) {
-
-  $("#header_text_about.menu_header_text").css({
-    "color": "red"
-  });
-}
-
-*/
-
-
     if ((url.endsWith(".com/") || url.endsWith("index.html") || url.endsWith(":4000/")) && (width >= scrollWidthLimit)) {
 
-        $(".header_box").css({
-            "display": "block"
-        });
-        $(".header_box_small").css({
-            "display": "none"
-        });
         $(".logo_header_bar").css({
             "opacity": "0"
         });
-        $("#social_container_small").css({
+        $(".social_container_small").css({
             "opacity": "0"
         });
         homeBig = true;
     } else {
-        if (width < scrollWidthLimit) {
-            console.log("url('" + base + "/assets/banner_mobile.png')");
-            $(".header_box_small").css("backgroundImage", "url('" + base + "/assets/banner_mobile.png')");
-        }
-
-        $(".header_box").css({
-            "display": "none"
-        });
-        $(".header_box_small").css({
-            "display": "block"
-        });
-
         homeBig = false;
     }
 });
@@ -135,7 +75,7 @@ $(window).scroll(function() {
         }, 150);
 
         if (homeBig) {
-            $("#social_container_small").animate({
+            $(".social_container_small").animate({
                 opacity: 1.0,
             }, 150);
             $(".logo_header_bar").animate({
@@ -150,7 +90,7 @@ $(window).scroll(function() {
         }, 150);
 
         if (homeBig) {
-            $("#social_container_small").animate({
+            $(".social_container_small").animate({
                 opacity: 0.0,
             }, 150);
             $(".logo_header_bar").animate({
